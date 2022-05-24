6f8ba34bfc78fc4c32853edef46715679ea08f75
Maria Mejia <mejia.maria1998@gmail.com>
2022-05-24 09:12:53 -0500

    adding front end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import {RegisterComponent} from './register/register.component';
+import { IndexComponent } from './index/index.component';
+import { ProductComponent } from './product/product.component';
+import { DetailsComponent } from './product/details/details.component';
+import { WelcomeComponent } from './misc/welcome/welcome.component';
+import {AuthGuard} from './auth/auth.guard';
+import {LoginComponent} from './auth/login/login.component';
+import { TemplateComponent } from './validation/template/template.component';
+import { ReactiveComponent } from './validation/reactive/reactive.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'product', component: ProductComponent,canActivate:[AuthGuard] },
+  { path:'product/:pId', component:DetailsComponent,canActivate:[AuthGuard] },
+  { path:'template-form', component:TemplateComponent,canActivate:[AuthGuard] },
+  { path:'reactive-form', component:ReactiveComponent,canActivate:[AuthGuard] },
+  { path: 'login', component: LoginComponent },
+ //{ path: '', component: IndexComponent ,pathMatch:"full"},
+ { path: 'welcome', component: WelcomeComponent ,canActivate:[AuthGuard]},
+  //{ path: 'register', component: RegisterComponent },
+ {path:"**",redirectTo:"product" },
+  {path:'', redirectTo:"product" ,pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './auth/login/login.component';
+import  {RegisterComponent} from './register/register.component';
+import { IndexComponent } from './index/index.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import  {OtherComponent} from './login/other.component'
+import {PropertyBindingComponent} from './login/property-binding.component';
+import {EventBindingComponent} from './login/event-binding.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsPipe } from './product/products.pipe';
+import { RatingComponent } from './product/rating/rating.component';
+import { PanelBoxComponent } from './misc/panel-box/panel-box.component';
+import { DetailsComponent } from './product/details/details.component';
+import {HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './misc/welcome/welcome.component';
+import {AuthService} from './auth/auth.service';
+import { NavbarComponent } from './misc/navbar/navbar.component';
+import { TemplateComponent } from './validation/template/template.component';
+import { ReactiveComponent } from './validation/reactive/reactive.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    IndexComponent,
+    OtherComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,
+    ProductComponent,
+    ProductsPipe,
+    RatingComponent,
+    PanelBoxComponent,
+    DetailsComponent,
+    WelcomeComponent,
+    NavbarComponent,
+    TemplateComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
